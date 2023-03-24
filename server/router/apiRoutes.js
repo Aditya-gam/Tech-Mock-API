69a1fe4c54057381a00b312cc6dac7b3e300cc75
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-03-24 15:35:27 +0530

    Create apiRoutes.js

---
+++ server/router/apiRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+const apiRouter = Router();
+
+/** import all controllers */
+
+/** POST Methods */
+
+/** GET Methods */
+
+/** PUT Methods */
+
+export default apiRouter;
