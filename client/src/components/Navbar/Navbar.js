e0bb132e84493a546b1f87f65149225182a207f5
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-03-24 16:15:47 +0530

    Added functionality to logout button

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,22 @@
 // import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 // import MenuItems from "./MenuItems";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   // const [active, setActive] = useState(false);
 
   // const showMenu = () => {
   //   setActive(!active);
   // };
+  // logout handler function
+  function userLogout() {
+    localStorage.removeItem("token"); // remove token from localstorage
+    navigate("/username");
+  }
   return (
-    <div className=" w-full flex justify-between p-4 items-center">
+    <div className="w-full flex justify-between p-4 items-center">
       <div className="text-2xl font-bold text-center">
         <h1>
           <span className="block uppercase text-2xl">logo</span>
@@ -27,7 +33,7 @@ const Navbar = () => {
             <Link to="/username">Login</Link>
           </li>
           <li>
-            <Link to="/">Logout</Link>
+            <button onClick={userLogout}>Logout</button>
           </li>
         </ul>
         {/* <MenuItems /> */}
