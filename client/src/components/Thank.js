72158330662660af9e04b99e72ae4cb0c94626c3
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-05-16 09:51:08 +0530

    Added URL to be displayed

--- client/src/components/Thank.js
+++ client/src/components/Thank.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 import Navbar from "./Navbar/Navbar";
 
 const Thank = () => {
+  const getUserID = () => {
+    const token = localStorage.getItem("token"); // get token from localstorage
+    if (!token) return Promise.reject("Cannot find Token"); // if token not found
+    let decode = jwt_decode(token); // decode token
+    return decode.userId;
+  };
   return (
     <section className="thank p-20">
       <Navbar />
@@ -11,6 +18,11 @@ const Thank = () => {
 
       <p className="text-lg font-mono">
         The request has been submitted succesfully
+      </p>
+      <p className="text-lg font-mono">
+        You can make a request to: http://localhost:8080/req/${getUserID()}/{" "}
+        <br />
+        Add your endpoint after last slash
       </p>
       <Link to="/">
         <button className="bg-gray-500 py-2 mt-6 px-6 text-white rounded-xl shadow-md">
