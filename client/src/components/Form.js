c252e8bd58b5438edff133e2fc17ed1b524f4819
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-05-16 09:29:32 +0530

    Added formik validation and onsubmit function

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,50 +1,31 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useFormik } from "formik";
+import React, { useState, useEffect } from "react";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-import jwt_decode from "jwt-decode";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../helper/axios";
 
 import Navbar from "./Navbar/Navbar";
-// import TestNavbar from "./Navbar/TestNavbar";
 
-const Form = () => {
-  const navigate = useNavigate(); // Navigate hook
-  const [inputFields, setInputFields] = useState([
-    {
-      parameter: "",
-      Value: "",
-    },
-  ]);
+const validationSchema = Yup.object().shape({
+  requestType: Yup.string().required("Request type is required"),
+  endpointUrl: Yup.string().required("Endpoint URL is required"),
+  responseBody: Yup.string().required("Response body is required"),
+  responseCode: Yup.number()
+    .required("Required")
+    .min(100, "Must be greater than or equal to 100")
+    .max(600, "Must be less than or equal to 600"),
+});
 
-  const validationSchema = Yup.object().shape({
-    requestType: Yup.string().required("Request Type is required"),
-    apiName: Yup.string(),
-    endpointUrl: Yup.string().required("Endpoint URL is required"),
-  });
+const MyForm = () => {
+  const navigate = useNavigate();
+  // const [formData, setFormData] = useState({});
 
-  const addInputFields = () => {
-    setInputFields([
-      ...inputFields,
-      {
-        parameter: "",
-        Value: "",
-      },
-    ]);
-  };
-
-  const removeInputFields = (index) => {
-    const rows = [...inputFields];
-    rows.splice(index, 1);
-    setInputFields(rows);
-  };
-
-  const handleChange = (index, event) => {
-    const { name, value } = event.target;
-    const list = [...inputFields];
-    list[index][name] = value;
-    setInputFields(list);
-  };
+  // useEffect(() => {
+  //   setFormData({});
+  //   console.log(formData);
+  // }, [formData]);
 
   const getUserID = () => {
     const token = localStorage.getItem("token"); // get token from localstorage
@@ -53,199 +34,139 @@ const Form = () => {
     return decode.userId;
   };
 
-  /** FORM VALIDATION AND SUBMIT */
-  const formik = useFormik({
-    initialValues: {
-      requestType: "",
-      apiName: "",
-      endpointUrl: "",
-      response_body: "",
-    },
-    validate: validationSchema,
-    validateOnBlur: false,
-    validateOnChange: false,
-    onSubmit: async (values) => {
+  const onSubmit = async (values) => {
+    // e.preventDefault();
+    try {
       const userId = await getUserID();
-      const payload = { userId, ...values };
-      try {
-        await axios.post("/api/createApiData", payload);
-        navigate("/thanks");
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  });
+      const payload = { ...values, userId };
+      console.log(payload);
+      alert("Form submitted successfully", payload);
+      await axiosInstance.post("/api/createApiData", payload);
+      navigate("/thanks");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
-    <div className="form-wrap ">
-      {/* <TestNavbar /> */}
+    <div className="form-wrap">
       <Navbar />
+      <h1 className="text-3xl font-bold text-center my-8">My Form</h1>
+
       <div className="w-full max-w-xl m-auto mt-10 ">
-        <form
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 font-serif "
-          onSubmit={formik.handleSubmit}
+        <Formik
+          initialValues={{
+            requestType: "",
+            endpointUrl: "",
+            responseBody: "",
+            responseCode: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
         >
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-lg font-bold mb-2"
-              htmlFor="username"
-            >
-              Select the request type
-            </label>
-            <div className="inline-block relative w-64">
-              <select
-                {...formik.getFieldProps("requestType")}
-                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              >
-                <option disabled selected hidden value="">
+          {({ errors, touched }) => (
+            <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 font-serif">
+              <div className="mb-4">
+                <label
+                  htmlFor="requestType"
+                  className="block text-gray-700 text-lg font-bold mb-2"
+                >
                   Request Type
-                </option>
-                <option className="hover:bg-red-200">POST</option>
-                <option> GET</option>
-                <option> PUT</option>
-                <option> PATCH</option>
-                <option> DELETE</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
+                </label>
+                <div className="inline-block relative w-64">
+                  <Field
+                    as="select"
+                    name="requestType"
+                    id="requestType"
+                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  >
+                    <option disabled selected hidden value="">
+                      Select a request type
+                    </option>
+                    <option className="hover:bg-red-200" value="GET">
+                      GET
+                    </option>
+                    <option className="hover:bg-red-200" value="POST">
+                      POST
+                    </option>
+                    <option className="hover:bg-red-200" value="PUT">
+                      PUT
+                    </option>
+                    <option className="hover:bg-red-200" value="PATCH">
+                      PATCH
+                    </option>
+                    <option className="hover:bg-red-200" value="DELETE">
+                      DELETE
+                    </option>
+                  </Field>
+                  {errors.requestType && touched.requestType ? (
+                    <div className="bg-red-500">{errors.requestType}</div>
+                  ) : null}
+                </div>
               </div>
-            </div>
-          </div>
-
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-lg font-bold mb-2"
-              htmlFor="username"
-            >
-              Select the API
-            </label>
-            <div className="inline-block relative w-64">
-              <select
-                {...formik.getFieldProps("apiName")}
-                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              <div className="mb-4">
+                <label
+                  htmlFor="endpointUrl"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Endpoint URL
+                </label>
+                <Field
+                  type="text"
+                  name="endpointUrl"
+                  id="endpointUrl"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                {errors.endpointUrl && touched.endpointUrl ? (
+                  <div>{errors.endpointUrl}</div>
+                ) : null}
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="responseBody"
+                  className="block text-gray-700 font-bold mb-2 mt-4"
+                >
+                  Response Body
+                </label>
+                <Field
+                  as="textarea"
+                  name="responseBody"
+                  id="responseBody"
+                  class="block text-black p-2.5 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+                {errors.responseBody && touched.responseBody ? (
+                  <div>{errors.responseBody}</div>
+                ) : null}
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="endpointUrl"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Response Code
+                </label>
+                <Field
+                  type="text"
+                  name="responseCode"
+                  id="responseCode"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                {errors.responseCode && touched.responseCode ? (
+                  <div>{errors.responseCode}</div>
+                ) : null}
+              </div>
+              <button
+                type="submit"
+                className="bg-gray-500 py-2 mt-6 px-6 text-white rounded-xl shadow-md"
               >
-                <option disabled hidden selected value="">
-                  API Name
-                </option>
-                <option className="hover:bg-red-200">API Name</option>
-                <option> API Name</option>
-                <option> API Name</option>
-                <option> API Name</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              for="username"
-            >
-              Endpoint URL
-            </label>
-            <input
-              {...formik.getFieldProps("endpointUrl")}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="url"
-              type="text"
-            />
-          </div>
-
-          <div className="container mx-auto">
-            <div className="flex flex-col">
-              {inputFields.map((data, index) => {
-                const { parameter, Value } = data;
-                return (
-                  <div className="flex flex-row my-3" key={index}>
-                    <div className="flex flex-col w-1/2 mr-2">
-                      <div className="form-group">
-                        <label className="block text-gray-700 font-bold mb-2">
-                          Enter the test data
-                        </label>
-                        <input
-                          type="text"
-                          name="parameter"
-                          value={parameter}
-                          onChange={(event) => handleChange(index, event)}
-                          placeholder="parameter"
-                          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                      </div>
-                    </div>
-                    <div className="flex flex-col w-1/2">
-                      <div className="form-group relative">
-                        <label className="block text-gray-700 font-bold mb-8">
-                          {" "}
-                        </label>
-                        <input
-                          type="value"
-                          name="Value"
-                          value={Value}
-                          onChange={(event) => handleChange(index, event)}
-                          placeholder="value"
-                          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                      </div>
-                    </div>
-                    {inputFields.length > 1 && (
-                      <button
-                        type="button"
-                        onClick={() => removeInputFields(index)}
-                        className="cross mt-10 text-white"
-                      >
-                        &#10005;
-                      </button>
-                    )}
-                  </div>
-                );
-              })}
-              <div className="flex flex-row justify-start ">
-                <button
-                  type="button"
-                  onClick={addInputFields}
-                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline cross "
-                >
-                  &#10010;
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <label className="block text-gray-700 font-bold mb-2 mt-4">
-            Response body
-          </label>
-          <textarea
-            {...formik.getFieldProps("response_body")}
-            id="message"
-            rows="4"
-            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          ></textarea>
-
-          <Link>
-            <button
-              type="submit "
-              className="bg-gray-500 py-2 mt-6 px-6 text-white rounded-xl shadow-md"
-            >
-              Submit
-            </button>
-          </Link>
-        </form>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
 };
 
-export default Form;
+export default MyForm;
