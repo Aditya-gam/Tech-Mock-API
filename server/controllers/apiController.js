19c07ad80225a39e1a6c07181d696e502e5271cb
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2023-05-26 16:31:50 +0530

    Changed to serachapi controller to get request data from URL

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -4,11 +4,17 @@ import ApiModel from "../model/Api.model.js";
 export async function createApiData(req, res) {
   try {
     // Extract data from the request body
-    const { _id, request, requestBody, endpoint, responseCode, responseBody } =
-      req.body;
+    const {
+      userId: userId,
+      requestType: request,
+      requestBody: requestBody,
+      endpointUrl: endpoint,
+      responseCode: responseCode,
+      responseBody: responseBody,
+    } = req.body;
     // Create a new document with the extracted data
     const apiData = new ApiModel({
-      _id,
+      userId,
       request,
       requestBody,
       endpoint,
@@ -27,49 +33,60 @@ export async function createApiData(req, res) {
 
 // Controller function to retrieve data from the database
 export async function getApiData(req, res) {
-  try {
-    // Query the database to get all the documents
-    const apiData = await ApiModel.find();
-    // Return the data as a response
-    res.status(200).send(apiData);
-  } catch (error) {
-    // Return an error response if something goes wrong
-    res.status(500).send({ error: error.message });
-  }
-  // const { _id } = req.params;
   // try {
-  //   const data = await Api.findById(_id);
-  //   if (!data) {
-  //     return res.status(404).json({ error: 'Data not found' });
-  //   }
-  //   return res.status(200).json({ data });
+  //   // Query the database to get all the documents
+  //   const apiData = await ApiModel.find();
+  //   // Return the data as a response
+  //   res.status(200).send(apiData);
   // } catch (error) {
-  //   console.error(error);
-  //   return res.status(500).json({ error: 'Internal server error' });
+  //   // Return an error response if something goes wrong
+  //   res.status(500).send({ error: error.message });
   // }
+  const { userId } = req.query;
+  try {
+    const data = await Api.findById(userId);
+    if (!data) {
+      return res.status(404).json({ error: "Data not found" });
+    }
+    return res.status(200).json({ data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 }
 
 // Controller function to search for data in the database
 export async function searchApiData(req, res) {
   try {
     // Extract data from the request body
-    const { _id, request, requestBody, endpoint } = req.body;
+    // const { request: requestType, endpoint: endpointUrl } = req.query;
+    const userId = req.params.userId;
+    const endpointUrl = "/" + req.params[0];
+    const requestType = req.method;
+    console.log(userId, endpointUrl, requestType);
+    // return;
     // Define the query parameters
+    // res.status(200).json({ requestType, endpointUrl, userId });
     const query = {
-      _id,
-      request,
-      endpoint,
+      userId: userId,
+      request: requestType,
+      endpoint: endpointUrl,
     };
-    if (request === "POST" || request === "PUT") {
-      query.requestBody = requestBody;
-    }
+    // if (request === "POST" || request === "PUT") {
+    //   query.requestBody = requestBody;
+    // }
     // Search the database for matching documents
-    const apiData = await ApiModel.find(query, {
+    const apiData = await ApiModel.findOne(query, {
       responseCode: 1,
       responseBody: 1,
     });
+    // console.log(
+    //   "🚀 ~ file: apiController.js:81 ~ searchApiData ~ apiData:",
+    //   apiData
+    // );
+
     // Return the data as a response
-    res.status(200).send(apiData);
+    res.status(apiData?.responseCode).send(apiData?.responseBody);
   } catch (error) {
     // Return an error response if something goes wrong
     res.status(500).send({ error: error.message });
